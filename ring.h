bad533cccd149ba078806a708ace257edb6974cb
Devansh Mittal <demi1671@colorado.edu>
2018-11-17 00:46:14 -0700

    Added function prototypes

--- ring.h
+++ ring.h
@@ -10,7 +10,12 @@ typedef struct
 	int Outi;	//front
 } ring_t;
 
-ring_t* init(int length);
+ring_t *ring;
+ring_t **ring_collector;
+
+ring_t** create_buffers(unsigned int ring_count);
+int init(int length, unsigned int ring_tracker);
+void select_buffer(unsigned int present_ring);
 int insert_data(ring_t *ring, char data);
 int remove_data(ring_t *ring, char *data);
 int entries(ring_t *ring);
