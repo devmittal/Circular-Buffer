5f63b6ffdbbec7e2b453f89103325b16df1714e7
Devansh Mittal <demi1671@colorado.edu>
2018-11-17 12:13:37 -0700

    Added folder structure for header files

---
+++ inc/ring.h
@@ -0,0 +1,24 @@
+#ifndef RING_H
+#define RING_H
+
+
+typedef struct
+{
+	char *Buffer;
+	int Length;
+	int Ini;	//Rear
+	int Outi;	//front
+} ring_t;
+
+ring_t *ring;
+ring_t **ring_collector;
+
+ring_t** create_buffers(unsigned int ring_count);
+int init(int length, unsigned int ring_tracker);
+int select_buffer(unsigned int present_ring);
+int insert_data(ring_t *ring, char data);
+int remove_data(ring_t *ring, char *data);
+int entries(ring_t *ring);
+
+#endif
+
